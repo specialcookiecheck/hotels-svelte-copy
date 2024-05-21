003b9a0363bc65d6a3ae853278d910cc06acdb7a
spcialcookiecheck <vincentgroeneveld_@hotmail.com>
2024-05-21 06:09:49 -0700

    trying settings change

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
+// /** @type {import('@sveltejs/kit').Config} */
 const config = {
     extensions: [".svelte"],
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
